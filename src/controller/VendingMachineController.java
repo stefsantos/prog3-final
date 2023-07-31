b55e31869e4eab6dbbad662f43e1a5790351d327
Sn3s <84768613+Sn3s@users.noreply.github.com>
2023-08-01 01:42:37 +0800

    asdf

--- src/controller/VendingMachineController.java
+++ src/controller/VendingMachineController.java
@@ -1,4 +1,9 @@
 package controller;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import model.Item;
 import model.VendingMachineModel;
 import view.MainMenu;
 import view.SpecialVendingMachineView;
@@ -17,8 +22,10 @@ public class VendingMachineController {
         mainMenu.setVisible(true);
     }
 
-    public void createRegularVendingMachine(int numRows) {
+   public void createRegularVendingMachine(int numRows) {
         this.model = new VendingMachineModel(numRows);
+        int maxStock = promptMaxStockForAllItems(); // Prompt the user to enter maximum stock for all items
+        setMaxStockForAllItems(maxStock); // Set the maximum stock for all items
         VendingMachineView vendingMachineView = new VendingMachineView(model);
         vendingMachineView.setVisible(true);
         vendingMachineView.updateBalanceLabel();
@@ -26,8 +33,46 @@ public class VendingMachineController {
 
     public void createSpecialVendingMachine(int numRows) {
         this.model = new VendingMachineModel(numRows);
+        int maxStock = promptMaxStockForAllItems(); // Prompt the user to enter maximum stock for all items
+        setMaxStockForAllItems(maxStock); // Set the maximum stock for all items
         SpecialVendingMachineView specialVendingMachineView = new SpecialVendingMachineView(model);
         specialVendingMachineView.setVisible(true);
         specialVendingMachineView.updateBalanceLabel();
+    }
+
+    private int promptMaxStockForAllItems() {
+        String input = JOptionPane.showInputDialog(
+                null,
+                "Enter the maximum stock for all items:",
+                "Maximum Stock",
+                JOptionPane.QUESTION_MESSAGE
+        );
+        if (input != null) {
+            try {
+                int maxStock = Integer.parseInt(input);
+                return maxStock;
+            } catch (NumberFormatException e) {
+                // Handle invalid input (not a valid integer)
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Invalid input. Please enter a valid integer.",
+                        "Invalid Input",
+                        JOptionPane.WARNING_MESSAGE
+                );
+                // Retry the input
+                return promptMaxStockForAllItems();
+            }
+        } else {
+            // User canceled the prompt, exit the program
+            System.exit(0);
+            return 0; // Not necessary, but added for clarity
+        }
+    }
+
+    private void setMaxStockForAllItems(int maxStock) {
+        List<Item> items = model.getItems();
+        for (Item item : items) {
+            item.setMaxStock(maxStock);
+        }
     }
 }
