81c920303ed073e87b86bff73f929b780660c0ea
Sn3s <84768613+Sn3s@users.noreply.github.com>
2023-08-01 21:16:39 +0800

    custom window has a button now

--- src/view/SpecialVendingMachineView.java
+++ src/view/SpecialVendingMachineView.java
@@ -78,25 +78,74 @@ public class SpecialVendingMachineView extends VendingMachineView {
     // Custom window class for the "Custom" button
     private class CustomItemWindow extends JFrame {
         public CustomItemWindow() {
-            setTitle("Custom Item Selection");
+            setTitle("Custom Item");
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-            setupGUI();
-            pack();
-            setLocationRelativeTo(null);
-        }
-
-        private void setupGUI() {
-            JPanel mainPanel = new JPanel(new GridLayout(model.getNumRows(), 1));
-
+            setPreferredSize(new Dimension(400, 300)); // Set the preferred size to half of the regular vending machine window
+            setLayout(new BorderLayout());
+    
+            // Create a scroll pane to hold the custom item panels
+            JScrollPane scrollPane = new JScrollPane();
+            add(scrollPane, BorderLayout.CENTER);
+    
+            JPanel mainPanel = new JPanel(new GridLayout(0, 1));
+    
             List<Item> items = model.getItems();
             for (int i = 0; i < items.size(); i++) {
                 Item item = items.get(i);
-                ItemPanel itemPanel = new ItemPanel(item, true, SpecialVendingMachineView.this); // Pass `SpecialVendingMachineView.this` as the reference
-                mainPanel.add(itemPanel);
+                if (!item.getName().isEmpty()) { // Check if the item name is not empty
+                    ItemCustomPanel itemCustomPanel = new ItemCustomPanel(item);
+                    mainPanel.add(itemCustomPanel);
+                }
             }
-
-            add(mainPanel);
+    
+            scrollPane.setViewportView(mainPanel); // Add the main panel to the scroll pane
+    
+            // Create the "Create Custom Milk Tea" button
+            JButton createCustomMilkTeaButton = new JButton("Create Custom Milk Tea");
+            createCustomMilkTeaButton.addActionListener(new CreateCustomMilkTeaListener());
+            mainPanel.add(createCustomMilkTeaButton); // Add the button to the main panel
+    
+            pack(); // Adjust the window size based on the contents
+            setLocationRelativeTo(null);
         }
-    }
+    
+    
+        private class ItemCustomPanel extends JPanel {
+            private Item item;
+            private JTextField stockField;
+    
+            public ItemCustomPanel(Item item) {
+                this.item = item;
+                setupPanel();
+            }
+    
+            private void setupPanel() {
+                setLayout(new BorderLayout());
+    
+                JLabel itemLabel = new JLabel(getItemLabelText());
+                add(itemLabel, BorderLayout.CENTER);
+    
+                stockField = new JTextField();
+                stockField.setPreferredSize(new Dimension(60, 30));
+                add(stockField, BorderLayout.EAST);
+            }
+    
+            private String getItemLabelText() {
+                return "<html><br><b>Name:</b> " + item.getName() +
+                        "<br><b>Stock:</b> " + item.getStock() + "/" + item.getMaxStock() +
+                        "<br><b>Calories:</b> " + item.getCalories() +
+                        "<br><b>Price:</b> P" + String.format("%.2f", item.getPrice()) + "<br></html>";
+            }
+        }
+
+        private class CreateCustomMilkTeaListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Implement the logic to handle the "Create Custom Milk Tea" button
+                // For example, you can display a new window or dialog for customizing milk tea options.
+                // Replace the following showMessageDialog with your custom logic.
+                JOptionPane.showMessageDialog(CustomItemWindow.this, "Create Custom Milk Tea Button Pressed!");
+            }
+        }
+    }    
 }
